ff34355517df27bd17942b384318812eb70586ae
mayureshzende <mayureshzende@gmail.com>
2022-05-16 15:31:26 +0530

    added tree recursion

---
+++ easy/isSameTree.js
@@ -0,0 +1,12 @@
+var isSameTree = function (p, q) {
+  /* 
+ base case 
+ */
+  if (p === null && q === null) return true;
+
+  if ((p === null && q !== null) || (q === null && p !== null)) return false;
+
+  if (p.val !== q.val) return false;
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
