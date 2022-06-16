e66bab0fcc101c9064f8f04e3c8365cbdd2a7a3f
Mayuresh Zende <mayureshzende@gmail.com>
2022-06-16 12:40:30 +0530

    started with linked list

---
+++ Topics/LinkedLists/removeNFromLast.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+  let dummy = new ListNode(0);
+  dummy.next = head;
+  let fast = dummy,
+    slow = dummy;
+
+  for (let i = 1; i <= n + 1; i++) {
+    fast = fast.next;
+  }
+
+  while (fast !== null) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+
+  slow.next = slow.next.next;
+
+  return dummy.next;
+};
