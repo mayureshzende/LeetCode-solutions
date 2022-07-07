6829926626b415ee5af22e547250a5211b531dd4
Mayuresh Zende <mayureshzende@gmail.com>
2022-07-07 19:25:06 +0530

    starting with dfs

--- algorithms/slidingWindow/permutationOfString.js
+++ algorithms/slidingWindow/permutationOfString.js
@@ -6,35 +6,41 @@
 var checkInclusion = function (s1, s2) {
   if (s1.length > s2.length || !s2.length) return false;
   if (!s1.length) return true;
-  let charArr = new Array(26).fill(0);
-  let i = 0;
-  let j = 0;
-  let total_count = s1.length;
+  let s1Arr = new Array(26).fill(0);
+  let s2Arr = new Array(26).fill(0);
+  let sc2 = [...s2];
+
   for (let i = 0; i < s1.length; i++) {
-    let curIdx = s1[i].charCodeAt() - 97;
-    charArr[curIdx]++;
+    s1Arr[s1[i].charCodeAt() - 97]++;
   }
-  while (j < s2.length) {
-    let curIdx = s2[j++].charCodeAt() - 97;
-
-    if (charArr[curIdx] > 0) {
-      total_count--;
-      charArr[curIdx]--;
-    }
-    if (total_count === 0) return true;
-
-    if ((j - i === s1.length && charArr[curIdx] >= 0, i++)) {
-      charArr[curIdx]++;
-      total_count++;
-    }
+  for (let i = 0; i < s2.length; i++) {
+    s2Arr[s2[i].charCodeAt() - 97]++;
   }
 
-  //   console.log(charArr);
+  for (let i = s1.length - 1; i < sc2.length; i++) {
+    s2Arr[sc2[i].charCodeAt() - 97]++;
+    if (isEqual(s2Arr, s1Arr)) {
+      return true;
+    }
+    s2Arr[sc2[i - s1.length + 1].charCodeAt() - 97]--;
+  }
+  console.log(s1Arr);
+  console.log(s2Arr);
+
   return false;
 };
 
-console.log(checkInclusion("abc", "eidbacooo"), true);
-console.log(checkInclusion("ab", "eidboaoo"), false);
+function isEqual(s1, s2) {
+  for (let i = 0; i < 26; i++) {
+    if (s1[i] !== s2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(checkInclusion("ab", "eidbacooo"), true);
+// console.log(checkInclusion("ab", "eidboaoo"), false);
 //     int[] map = new int[26];
 //     for(char c : s1.toCharArray()) map[c - 'a']++;
 //     int j = 0, i = 0;
--- algorithms/twoPointers.js/removeFromEndofList.js
+++ algorithms/twoPointers.js/removeFromEndofList.js
@@ -27,5 +27,5 @@ var removeNthFromEnd = function (head, n) {
   }
 
   slow.next = slow.next.next;
-  return dummy;
+  return dummy.next;
 };
